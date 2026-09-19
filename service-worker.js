81241dffd207d1c089297a6bd4087547ad50b975
madsonpersonal-max <madsonpersonal@hotmail.com>
2026-09-19 17:21:13 -0300

    Revise service worker for cache versioning and fetch
    Updated service worker to new cache version and improved fetch handling.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,40 @@
-const CACHE="mundo-da-elisa-v11";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"])).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = "mundo-da-elisa-v12-3";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll(["./index.html", "./manifest.webmanifest"])
+    )
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache =>
+            cache.put("./index.html", copy)
+          );
+          return response;
+        })
+        .catch(() =>
+          caches.match("./index.html")
+        )
+    );
+  }
+});
